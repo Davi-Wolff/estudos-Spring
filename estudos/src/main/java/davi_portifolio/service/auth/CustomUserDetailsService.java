55db1ea8ee07f7d3dbda19d89b0962c8afc84e19
Davi-Wolff <daviwolffdeabreu@gmail.com>
2026-09-24 16:07:35 -0300

    Update dia 14.2: refatorei CustomUserDetailsService e JwtService para um pasta a parte

--- estudos/src/main/java/davi_portifolio/service/auth/CustomUserDetailsService.java
+++ estudos/src/main/java/davi_portifolio/service/auth/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package davi_portifolio.service;
+package davi_portifolio.service.auth;
 
 import davi_portifolio.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
--- estudos/src/main/java/davi_portifolio/service/auth/JwtService.java
+++ estudos/src/main/java/davi_portifolio/service/auth/JwtService.java
@@ -1,4 +1,4 @@
-package davi_portifolio.service;
+package davi_portifolio.service.auth;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
