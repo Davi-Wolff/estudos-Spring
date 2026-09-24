49629ea335c67a42bc0570b30f77d37fea8aec0d
Davi-Wolff <daviwolffdeabreu@gmail.com>
2026-09-24 16:06:48 -0300

    Update dia 14: Criação do EmailService e inicio da mensageria via email

---
+++ estudos/src/main/java/davi_portifolio/service/email/EmailService.java
@@ -0,0 +1,48 @@
+package davi_portifolio.service.email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableAsync
+public class EmailService {
+
+    @Autowired
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    private final String from;
+
+    public EmailService(JavaMailSender mailSender,
+                        @Value("${spring.mail.username}") String from) {
+        this.mailSender = mailSender;
+        this.from = from;
+    }
+
+    @Async
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom(from);
+
+        try {
+            mailSender.send(message);
+        }catch (MailException e) {
+            log.error("Falha ao enviar email para {}", to, e);
+        }
+    }
+
+}
